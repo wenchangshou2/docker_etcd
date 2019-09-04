470e7cf9d55f145ef8344bd872368db7af0dffaa
wenchangshou <wenchangshou@live.cn>
2019-09-04 14:33:36 +0800

    添加etcd 3.4版本

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+ETCD_CMD="/bin/etcd \
+   --data-dir /data \
+  --name s1 \
+  --listen-client-urls http://0.0.0.0:2379 \
+  --advertise-client-urls http://0.0.0.0:2379 \
+  --listen-peer-urls http://0.0.0.0:2380 \
+  --initial-advertise-peer-urls http://0.0.0.0:2380 \
+  --initial-cluster s1=http://0.0.0.0:2380 \
+  --initial-cluster-token tkn \
+  --initial-cluster-state new \
+  --log-level info \
+  --logger zap \
+  --log-outputs stderr
+ "
+
+echo -e "Running '$ETCD_CMD'\nBEGIN ETCD OUTPUT\n"
+exec $ETCD_CMD
